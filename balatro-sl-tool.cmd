41b97bf6100fa35bafca897debe116342f18ac4c
chunqiuyiyu <chunqiuyiyu@outlook.com>
2024-02-26 19:44:39 +0800

    feat: support manually save/load functions

--- balatro-sl-tool.cmd
+++ balatro-sl-tool.cmd
@@ -12,9 +12,11 @@ title Balatro S/L Tool
 
 echo ============================
 echo 欢迎使用 Balatro 存档同步工具
-echo 工具每 20 秒检查一次存档更新
+echo 工具每 5 秒检查一次存档更新
+echo.
 @echo off
-echo 按 A 键回档
+echo 请在游戏返回主菜单后使用以下按键
+echo A 键回档，S 键手动存档，D 键手动回档
 echo ============================
 echo.
 
@@ -22,6 +24,10 @@ echo.
 set Profile=1
 set FileName=C:\Users\%USERNAME%\AppData\Roaming\Balatro\%Profile%\save.jkr
 set FileTime=-
+set ManuallyFileTime=-
+for %%X in (C:\Users\%USERNAME%\Documents\BalatroSL\save-manually.jkr) do (
+set ManuallyFileTime=%%~tX
+)
 
 echo 正在检查存档更新...
 
@@ -30,7 +36,7 @@ if exist "%FileName%" (
     for %%X in (%FileName%) do (
         if  "%FileTime%" NEQ "%%~tX" (
             rem copy the file to another location
-            xcopy %FileName% C:\Users\%USERNAME%\Documents\BalatroSL\save.jkr* /Y > null
+            xcopy %FileName% C:\Users\%USERNAME%\Documents\BalatroSL\save.jkr* /Y
             echo 存档已更新于 %%~tX
             echo.
         )
@@ -41,18 +47,32 @@ if exist "%FileName%" (
 
 
 rem press A to load the save
-choice /c AB /t 3 /d B > null
+choice /c ASDB /t 3 /d B >nul 2>nul
 if %errorlevel% == 1 (
      echo.
      echo 正在回档...更新于 %FileTime%
-     xcopy C:\Users\%USERNAME%\Documents\BalatroSL\save.jkr C:\Users\%USERNAME%\AppData\Roaming\Balatro\%Profile%\ /Y > null
+     xcopy C:\Users\%USERNAME%\Documents\BalatroSL\save.jkr C:\Users\%USERNAME%\AppData\Roaming\Balatro\%Profile%\ /Y
      echo 回档完成
      echo.
 )
 if %errorlevel% == 2 (
+    echo.
+    xcopy %FileName% C:\Users\%USERNAME%\Documents\BalatroSL\save-manually.jkr* /Y > null
+    rem get the file time
+    echo 正在手动存档...更新于 %ManuallyFileTime%
+    echo.
+)
+if %errorlevel% == 3 (
+    echo.
+    echo 正在手动回档...更新于 %ManuallyFileTime%
+    xcopy C:\Users\%USERNAME%\Documents\BalatroSL\save-manually.jkr C:\Users\%USERNAME%\AppData\Roaming\Balatro\%Profile%\save.jkr* /Y
+    echo 回档完成
+    echo.
+)
+if %errorlevel% == 4 (
    rem
 )
-rem wait 20 seconds before checking again
-ping -n 21 localhost >nul 2>nul
+rem wait 5 seconds before checking again
+ping -n 6 localhost >nul 2>nul
 goto :loop
 
